8a64168c18a46831399ba42dfcc606bd7c02f2d5
ledoyen <ledoyen.loic@gmail.com>
2016-08-24 23:42:06 +0200

    ObjectMessage, MapMessage and generic JMS properties

---
+++ spring-automocker/src/main/java/com/github/ledoyen/automocker/tools/ThrowingBiConsumer.java
@@ -0,0 +1,19 @@
+package com.github.ledoyen.automocker.tools;
+
+import java.util.function.BiConsumer;
+
+@FunctionalInterface
+public interface ThrowingBiConsumer<T, U> {
+
+	void accept(T t, U u) throws Exception;
+
+	static <T, U> BiConsumer<T, U> silent(ThrowingBiConsumer<T, U> throwing) {
+		return (t, u) -> {
+			try {
+				throwing.accept(t, u);
+			} catch (Exception e) {
+				throw new LamdaLuggageException(e);
+			}
+		};
+	}
+}
