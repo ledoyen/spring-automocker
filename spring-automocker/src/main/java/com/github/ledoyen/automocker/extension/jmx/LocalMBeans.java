2a7b48b5624342a955f625f2ab1006641bc7bf51
ledoyen <ledoyen.loic@gmail.com>
2016-11-11 23:12:12 +0100

    add JMX utility

---
+++ spring-automocker/src/main/java/com/github/ledoyen/automocker/extension/jmx/LocalMBeans.java
@@ -0,0 +1,78 @@
+package com.github.ledoyen.automocker.extension.jmx;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
+
+import javax.management.Attribute;
+import javax.management.JMException;
+import javax.management.MBeanInfo;
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import com.github.ledoyen.automocker.tools.Guavas;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+public final class LocalMBeans {
+
+	private static final int CACHE_SIZE = 100;
+	private static final MBeanServer MBEAN_SERVER = ManagementFactory.getPlatformMBeanServer();
+	public static final LocalMBeans INSTANCE = new LocalMBeans();
+
+	private final LoadingCache<String, ObjectName> objectNameByLiterals = CacheBuilder.newBuilder()
+			.maximumSize(CACHE_SIZE)
+			.build(CacheLoader.from(Guavas.fromThrowing(LocalMBeans::getObjectName)));
+	private final LoadingCache<ObjectName, String[]> attributesByObjectNames = CacheBuilder.newBuilder()
+			.maximumSize(CACHE_SIZE)
+			.build(CacheLoader.from(Guavas.fromThrowing(LocalMBeans::getAttributeNames)));
+
+	private LocalMBeans() {
+	}
+
+	public Map<String, Object> listAttributes(String name) {
+		try {
+			ObjectName on = objectNameByLiterals.get(name);
+			return extractAttributes(on, attributesByObjectNames.get(on));
+		} catch (ExecutionException e) {
+			throw new IllegalArgumentException(e.getCause());
+		}
+	}
+
+	private static ObjectName getObjectName(String name) throws JMException {
+		ObjectName on = new ObjectName(name);
+		if (on.isPattern()) {
+			Set<ObjectName> names = MBEAN_SERVER.queryNames(on, null);
+			assertThat(names).as("Eligible MBeans")
+					.hasSize(1);
+			on = names.iterator()
+					.next();
+		}
+		return on;
+	}
+
+	private static String[] getAttributeNames(ObjectName name) throws JMException {
+		MBeanInfo info = MBEAN_SERVER.getMBeanInfo(name);
+		return Arrays.asList(info.getAttributes())
+				.stream()
+				.map(a -> a.getName())
+				.toArray(size -> new String[size]);
+	}
+
+	private Map<String, Object> extractAttributes(ObjectName on, String[] attributeNames) {
+		try {
+			return MBEAN_SERVER.getAttributes(on, attributeNames)
+					.asList()
+					.stream()
+					.collect(Collectors.toMap(Attribute::getName, Attribute::getValue));
+		} catch (JMException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+}
---
+++ spring-automocker/src/main/java/com/github/ledoyen/automocker/tools/Guavas.java
@@ -0,0 +1,14 @@
+package com.github.ledoyen.automocker.tools;
+
+import java.util.function.Function;
+
+public class Guavas {
+
+	public static <T, R> com.google.common.base.Function<T, R> fromThrowing(ThrowingFunction<T, R> throwing) {
+		return fromJava(ThrowingFunction.silent(throwing));
+	}
+
+	public static <T, R> com.google.common.base.Function<T, R> fromJava(Function<T, R> function) {
+		return t -> function.apply(t);
+	}
+}
